76a1ec9c971ddf963f4c35e798c6e2e0b98b2c8f
turt2live <travpc@gmail.com>
2014-05-20 21:43:57 -0600

    Misc updates

--- src/main/java/com/turt2live/dumbcoin/TopBalanceManager.java
+++ src/main/java/com/turt2live/dumbcoin/TopBalanceManager.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Copyright (C) 2014 Travis Ralston (turt2live)
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 package com.turt2live.dumbcoin;
 
 import com.turt2live.dumbcoin.util.BalanceQuicksort;
@@ -10,6 +27,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class TopBalanceManager {
 
     public static final class PlayerBalance implements ConfigurationSerializable, Cloneable {
+
         private String playerName;
         private double balance;
         protected int rank;
